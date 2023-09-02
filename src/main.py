c23ee5b6b5949f51a895b2536af2c972a239c70b
G <0020992@gmail.com>
2023-09-02 23:47:34 +0300

    added docstring

--- src/main.py
+++ src/main.py
@@ -2,12 +2,30 @@ from src.utils import load_json, get_recent_operations, get_executed,\
     format_date, format_amount, extract_card_info
 FILE_NAME = "data/operations.json"
 if __name__ == '__main__':
+    """
+        Этот скрипт загружает операции из JSON-файла, фильтрует и сортирует их, 
+        а затем выводит информацию о них в удобном формате.
+    """
+    # Загружаем операции из JSON-файла
     operations = load_json(FILE_NAME)
-    operations = get_executed(operations)
-    operations = get_recent_operations(operations)
-    for operation in operations:
-        print(format_date(operation))
-        print(extract_card_info(operation.get("from", None)), "->", operation["to"])
-        print(format_amount(operation["operationAmount"]))
-        print()
 
+    # Фильтруем операции, оставляя только выполненные
+    operations = get_executed(operations)
+
+    # Получаем пять самых последних выполненных операций
+    operations = get_recent_operations(operations)
+
+    # Выводим информацию о каждой операции
+    for operation in operations:
+
+        # Выводим дату и описание операции
+        print(format_date(operation))
+
+        # Выводим информацию о карте отправителя (если есть) и получателе
+        print(extract_card_info(operation.get("from", None)), "->", operation["to"])
+
+        # Выводим сумму операции и валюту
+        print(format_amount(operation["operationAmount"]))
+
+        # Пустая строка для разделения операций
+        print()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,24 +3,46 @@ import json
 from src.utils import format_amount, extract_card_info, format_date, get_recent_operations, \
     get_executed, load_json
 DATA_FILE = "tests/test_load_json.json"
+
+
 @pytest.fixture
 def sample_data():
+    """
+    Фиктивные данные для тестирования.
+    """
     with open(DATA_FILE, 'r') as json_file:
         return json.load(json_file)
 
+
 @pytest.fixture
 def sample_operation(sample_data):
+    """
+    Выбирает первую операцию из фиктивных данных для тестирования.
+    """
     return sample_data[0]
 
+
 def test_format_date(sample_operation):
+    """
+    Тестирование функции format_date.
+    Проверяет корректность форматирования даты и описания операции.
+    """
     assert format_date(sample_operation) == "08.02.2010 Перевод организации"
 
 
 def test_format_amount(sample_operation):
+    """
+    Тестирование функции format_amount.
+    Проверяет корректность форматирования суммы и валюты операции.
+    """
     assert format_amount(sample_operation["operationAmount"]) == "62654.30 USD"
 
 
 def test_extract_card_info():
+    """
+    Тестирование функции extract_card_info.
+    Проверяет корректность извлечения информации о карте отправителя.
+    """
     assert extract_card_info("Visa Classic 1203921041964079") == "Visa Classic 1203 92** **** 4079"
     assert extract_card_info("Счет 34616199494072692721") == "Счет **2721"
     assert extract_card_info("Visa Classic 120392001041964079") == "Неизвестный номер"
@@ -28,6 +50,10 @@ def test_extract_card_info():
 
 
 def test_get_recent_operations(sample_data):
+    """
+    Тестирование функции get_recent_operations.
+    Проверяет корректность выбора пяти последних операций.
+    """
     result_list = get_recent_operations(sample_data)
     assert len(result_list) == 5
     assert result_list[0]["id"] == 692008409
@@ -35,10 +61,18 @@ def test_get_recent_operations(sample_data):
 
 
 def test_get_executed(sample_data):
+    """
+    Тестирование функции get_executed.
+    Проверяет корректность выбора выполненных операций.
+    """
     result_list = get_executed(sample_data)
     assert len(result_list) == 5
 
 
 def test_load_json():
+    """
+    Тестирование функции load_json.
+    Проверяет корректность загрузки JSON-данных из файла.
+    """
     result_list = load_json(DATA_FILE)
     assert len(result_list) == 6
